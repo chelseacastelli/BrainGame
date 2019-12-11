f9c919b992cb94d6d174f963728ed980732cf840
chelseacastelli <chelsea.castelli@students.makeschool.com>
2019-12-10 16:44:08 -0800

    [UPDATE] User can make one guess

--- FinalProject/ViewController.swift
+++ FinalProject/ViewController.swift
@@ -11,43 +11,87 @@ import UIKit
 class ViewController: UIViewController {
     
     let possibleColors = ["blue", "red", "green", "orange", "yellow"]
-    let possibleUIColors = [UIColor.red, UIColor.blue, UIColor.green, UIColor.yellow, UIColor.orange]
+    let possibleUIColors = [UIColor.blue, UIColor.red, UIColor.green, UIColor.orange, UIColor.yellow]
     
     @IBOutlet weak var meaningLabel: UILabel!
     @IBOutlet weak var textColorLabel: UILabel!
+    @IBOutlet weak var answerIndicator: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var headerLabel: UILabel!
     
-
+    var gameTimer = Timer()
+    
+    var time: Int = 9 {
+        didSet {
+            timeLabel.text = String(time)
+        }
+    }
+    
+    var score: Int = 0 {
+        didSet {
+            scoreLabel.text = String(score)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
         generateMeaning()
+        gameTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.startGameTimer), userInfo: nil, repeats: true)
     }
-    
+     
     @IBAction func noTapped(_ sender: Any) {
-        
+        if possibleColors.firstIndex(of: meaningLabel.text!) != possibleUIColors.firstIndex(of: textColorLabel.textColor) {
+            answerIndicator.text = "Correct!"
+            answerIndicator.textColor = UIColor.green
+            score += 10
+        } else {
+            answerIndicator.text = "Wrong!"
+            answerIndicator.textColor = UIColor.red
+            score -= 10
+        }
+        answerIndicator.isHidden = false
     }
-    
+      
     @IBAction func yesTapped(_ sender: Any) {
-        
+        if possibleColors.firstIndex(of: meaningLabel.text!) == possibleUIColors.firstIndex(of: textColorLabel.textColor) {
+            answerIndicator.text = "Correct!"
+            answerIndicator.textColor = UIColor.green
+            score += 10
+        } else {
+            answerIndicator.text = "Wrong!"
+            answerIndicator.textColor = UIColor.red
+            score -= 10
+        }
+        answerIndicator.isHidden = false
     }
-    
+      
     func generateMeaning() {
+        answerIndicator.isHidden = true
+        
         let randomMeaning = Int.random(in: 0..<possibleColors.count)
         let randomTextColor = Int.random(in: 0..<possibleColors.count)
         let randomColor = Int.random(in: 0..<possibleUIColors.count)
-        
+          
         meaningLabel.text = possibleColors[randomMeaning]
         textColorLabel.text = possibleColors[randomTextColor]
         textColorLabel.textColor = possibleUIColors[randomColor]
     }
     
-//    func generateRandomColor() -> UIColor {
-//      let hue : CGFloat = CGFloat(arc4random() % 256) / 256 // use 256 to get full range from 0.0 to 1.0
-//      let saturation : CGFloat = CGFloat(arc4random() % 128) / 256 + 0.5 // from 0.5 to 1.0 to stay away from white
-//      let brightness : CGFloat = CGFloat(arc4random() % 128) / 256 + 0.5 // from 0.5 to 1.0 to stay away from black
-//
-//       return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: 1)
-//    }
+    @objc func startGameTimer() {
+        time -= 1
+        timeLabel.text = String(time)
+        
+        if time == 0 {
+            gameTimer.invalidate()
+            headerLabel.isHidden = true
+            answerIndicator.text = "Times up!  Your score: \(score)"
+            answerIndicator.textColor = UIColor.white
+            answerIndicator.isHidden = false
+        }
+    }
 
 }
 
